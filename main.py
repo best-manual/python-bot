8f375b7a0674ad2451df1f836265040ba3addb84
Артур Таран <astaran23@MacBook-Pro-Artur.local>
2022-11-18 14:46:59 +0300

    Настройка PythonAnywhere

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('mysite')
+        repo = git.Repo('python-bot')
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
@@ -18,5 +18,5 @@ def webhook():
 
 @app.route('/')
 def hello_world():
-    return 'Hello New World 2!'
+    return 'Hello New World 3!'
 
