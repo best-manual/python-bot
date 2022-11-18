67bda4d2cb39b644f8c58639880b50893c912fdd
Артур Таран <astaran23@MacBook-Pro-Artur.local>
2022-11-18 11:35:23 +0300

    Настройка деплоя на PythonAnywhere

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, json, request
 import logging
+import git
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
