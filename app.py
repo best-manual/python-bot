5f996f3eb842e716e0597ac1b1c4eb1d4e893efe
Артур Таран <astaran23@MacBook-Pro-Artur.local>
2022-11-18 11:42:01 +0300

    Настройка деплоя с Git в PythonAnywhere

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('path/to/git_repo')
+        repo = git.Repo('/best-manual/python-bot')
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
