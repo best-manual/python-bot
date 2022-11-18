f18bdba58f21d18dc9acb781d2475b6e1a818a02
Best Manual <best.manual.info@gmail.com>
2022-11-18 11:59:57 +0000

    Настройка PythonAnywhere

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('mysite')
+        repo = git.Repo('python-bot')
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
